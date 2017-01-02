fff5457820e91a06212a7a366edf6efb9269109f
Noam Ben Shitrit <noambs1@gmail.com>
2017-01-02 19:35:53 +0200

    implement Model interface

---
+++ app/src/main/java/com/openu/a2017_app1/models/MassAssignmentException.java
@@ -0,0 +1,13 @@
+package com.openu.a2017_app1.models;
+
+/**
+ * Created by noam on 02/01/2017.
+ */
+
+public class MassAssignmentException  extends RuntimeException{
+
+
+    public MassAssignmentException(String key) {
+        super(key);
+    }
+}
