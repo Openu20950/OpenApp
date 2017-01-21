6280ecfa440fe5cc3cbb42b6259ac1515606a5f6
Noam Ben Shitrit <noambs1@gmail.com>
2017-01-21 18:41:16 +0200

    add table name to models

--- app/src/main/java/com/openu/a2017_app1/models/PlaceImp.java
+++ app/src/main/java/com/openu/a2017_app1/models/PlaceImp.java
@@ -23,6 +23,7 @@ public class PlaceImp extends BasicModel implements Place {
      */
     public PlaceImp()
     {
+        this.table="Place";
         this.name="";
         this.placeLocation=new LocationPoint();
         this.phone="";
